cbc1eb5c0a7f61ee846bea7aa7f72a60c95d71bc
Jim Öqvist <jim@oqvist.org>
2019-11-28 13:54:57 +0100

    added CSV structure

--- mgmt_api/generic_object_export_import_users.sh
+++ mgmt_api/generic_object_export_import_users.sh
@@ -12,13 +12,22 @@ sed -i '1 s/^/"create","name","email","phoneNumber","color","authMethod","intern
 # Import users from CSV (if you are importing from a domain on a MDS change "SMC User" to the dommain name)
 mgmt_cli -r true -d "SMC User" -f json add generic-object --batch users.csv
 
+#### Example of the CSV output
+##  If you just want to import a list of users based on a CSV file this can be done by usng the add generic-object API endpoint in batch mode by formating the source csv file into a format like this:
+# "create","name","email","phoneNumber","color","authMethod","internalPassword","adminExpirationBaseData.create","adminExpirationBaseData.owned-object.expirationDate","userc.create","userc.owned-object.useGlobalEncryptionValues","userc.owned-object.ike.create"
+# "com.checkpoint.objects.classes.dummy.CpmiUser","Luna","","","BLACK","UNDEFINED","$(cpopenssl passwd -crypt -salt $(cpopenssl rand -base64 2) Mypaswd)","com.checkpoint.objects.classes.dummy.CpmiAdminExpirationBaseData","31-dec-2030","com.checkpoint.objects.classes.dummy.CpmiSpecificUserc",true,"com.checkpoint.objects.classes.dummy.CpmiSpecificUsercIke"
+# "com.checkpoint.objects.classes.dummy.CpmiUser","Liv","","","BLACK","UNDEFINED","$(cpopenssl passwd -crypt -salt $(cpopenssl rand -base64 2) Mypaswd)","com.checkpoint.objects.classes.dummy.CpmiAdminExpirationBaseData","10-Apr-2021","com.checkpoint.objects.classes.dummy.CpmiSpecificUserc",true,"com.checkpoint.objects.classes.dummy.CpmiSpecificUsercIke"
+# "com.checkpoint.objects.classes.dummy.CpmiUser","Jim","","","BLACK","UNDEFINED","$(cpopenssl passwd -crypt -salt $(cpopenssl rand -base64 2) Mypaswd)","com.checkpoint.objects.classes.dummy.CpmiAdminExpirationBaseData","10-Apr-2021","com.checkpoint.objects.classes.dummy.CpmiSpecificUserc",true,"com.checkpoint.objects.classes.dummy.CpmiSpecificUsercIke"
+# "com.checkpoint.objects.classes.dummy.CpmiUser","apiuser6","api6@user.local","00468118118","BLUE_1","INTERNAL_PASSWORD","$(cpopenssl passwd -crypt -salt $(cpopenssl rand -base64 2) Mypaswd)","com.checkpoint.objects.classes.dummy.CpmiAdminExpirationBaseData","10-Apr-2020","com.checkpoint.objects.classes.dummy.CpmiSpecificUserc",true,"com.checkpoint.objects.classes.dummy.CpmiSpecificUsercIke"
+#
+#
 ##
 # Add user example
 # mgmt_cli -r true -d "SMC User" -f json add generic-object create "com.checkpoint.objects.classes.dummy.CpmiUser" name "apiuser11" email "api6@user.local" phoneNumber "00468118118" color "BLUE_1" authMethod "INTERNAL_PASSWORD" internalPassword $(cpopenssl passwd -crypt -salt $(cpopenssl rand -base64 2) 'Mypaswd!') adminExpirationBaseData.create "com.checkpoint.objects.classes.dummy.CpmiAdminExpirationBaseData" adminExpirationBaseData.owned-object.expirationDate "10-Apr-2020" userc.create "com.checkpoint.objects.classes.dummy.CpmiSpecificUserc" userc.owned-object.useGlobalEncryptionValues "true" userc.owned-object.ike.create "com.checkpoint.objects.classes.dummy.CpmiSpecificUsercIke"
 ###
 #
 ###
-# The following lines are currently a work in process to change this into an onteractive script.
+# The following lines are currently a work in process to change this into an interactive script.
 # this is how a interactive script could look like when exporting users
 #read -p "Enter gateway name to add into RemoteAccess comunity: " varGW
 #read -p "Enter management server IP : " varMgtIP
