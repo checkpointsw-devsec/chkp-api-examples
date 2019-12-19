8ccc055fb34907dae302c753897ae99d4cc3cfdb
Jim Öqvist <jim@oqvist.org>
2019-12-19 23:32:41 +0100

    reformated file

--- mgmt_cli/gaia_api_set-password-policy.sh
+++ mgmt_cli/gaia_api_set-password-policy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!\bin\bash
 uid=admin
 pwd=vpn123
 mgmt_sid=$(mgmt_cli login user $uid password $pwd -f json | jq -r '.sid')
@@ -9,5 +9,21 @@ gw_and_srv=$(mgmt_cli show-gateways-and-servers details-level full --session-id 
 gaia_ip=$(echo "$gw_and_srv" | jq -r '.objects[] | select((.type != "CpmiGatewayCluster") and ."sic-status" == "communicating") | ."ipv4-address"')
 
 rm -f pasword_complexity_report_for_auditor.out
-mgmt_cli show hostname -m $gaia_ip --version 1.3 --context gaia_api --session-id $gaia_sid --format json | tee -a pasword_complexity_report_for_auditor.out
-mgmt_cli set-password-policy -m $gaia_ip --version 1.3 --context gaia_api --session-id $gaia_sid --format json password-history.check-history-enabled true password-history.repeated-history-length 12 password-strength.complexity 3 password-strength.minimum-length 12 | tee -a pasword_complexity_report_for_auditor.out
+mgmt_cli show-hostname \
+  -m $gaia_ip \
+  --version 1.3 \
+  --context gaia_api \
+  --session-id $gaia_sid \
+  --format json \
+| tee -a pasword_complexity_report_for_auditor.out
+mgmt_cli set-password-policy \
+  password-history.check-history-enabled true \
+  password-history.repeated-history-length 12 \
+  password-strength.complexity 3 \
+  password-strength.minimum-length 12 \
+  --management $gaia_ip \
+  --version 1.3 \
+  --context gaia_api \
+  --session-id $gaia_sid \
+  --format json \
+| tee -a pasword_complexity_report_for_auditor.out
