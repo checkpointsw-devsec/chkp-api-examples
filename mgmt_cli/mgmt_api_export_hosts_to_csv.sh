dacff4303e1eaded8349da1435ef3e30314d0d56
Jim Öqvist <jim@oqvist.org>
2019-12-20 00:34:16 +0100

    added delete_host.csv output

--- mgmt_cli/mgmt_api_export_hosts_to_csv.sh
+++ mgmt_cli/mgmt_api_export_hosts_to_csv.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Execute with: # curl_cli -kLs -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/jimoq/CHKP_api_examples/master/mgmt_cli/mgmt_api_export_hosts_to_csv.sh | bash
 echo Export hosts to CSV
+hosts=$(mgmt_cli -r true -f json show hosts details-level full)
+echo $hosts |  jq -r '.objects[]| [.name, ."ipv4-address", .color] | @csv' | tee add_hosts.csv
+echo $hosts |  jq -r '.objects[]| [.name] | @csv' | tee delete_hosts.csv
 mgmt_cli -r true -f json show hosts details-level full | jq -r '.objects[]| [.name, ."ipv4-address", .color] | @csv' | tee hosts.csv
 echo Add headers with parameter names to the CSV
-sed -i '1 s/^/"name","ipv4-address", "color"\n/' hosts.csv
-echo data is exported to hosts.csv
+sed -i '1 s/^/"name","ipv4-address", "color"\n/' add_hosts.csv
+sed -i '1 s/^/"name"\n/' delete_hosts.csv
+echo data is exported to add_hosts.csv and delete_hosts.csv
