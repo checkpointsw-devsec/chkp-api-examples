f37e28a5089eecf988ac21d6e8d29114d1f7c510
Jim Öqvist <jim@oqvist.org>
2019-12-20 00:21:20 +0100

    added descriptions in the end

--- mgmt_cli/mgmt_api_export_hosts_to_csv.sh
+++ mgmt_cli/mgmt_api_export_hosts_to_csv.sh
@@ -4,3 +4,4 @@ echo Export hosts to CSV
 mgmt_cli -r true -f json show hosts details-level full | jq -r '.objects[]| [.name, ."ipv4-address", .color] | @csv' | tee hosts.csv
 echo Add headers with parameter names to the CSV
 sed -i '1 s/^/"name","ipv4-address", "color"\n/' hosts.csv
+echo data is exported to hosts.csv
