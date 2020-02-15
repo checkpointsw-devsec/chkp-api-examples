32f3c2f55f995aec265fd930c0dd93091d23a732
Jim Öqvist <jim@oqvist.org>
2020-02-15 21:24:20 +0100

    store policy and rules as tf.orig

--- Terraform/Terraform_Demo_policy/download_terraform_demo.bat
+++ Terraform/Terraform_Demo_policy/download_terraform_demo.bat
@@ -4,9 +4,8 @@ cd Terraform_Demo_policy
 curl -kLs https://raw.githubusercontent.com/jimoq/CHKP_api_examples/master/Terraform/Terraform_Demo_policy/hosts.tf > hosts.tf
 curl -kLs https://raw.githubusercontent.com/jimoq/CHKP_api_examples/master/Terraform/Terraform_Demo_policy/main.tf > main.tf
 curl -kLs https://raw.githubusercontent.com/jimoq/CHKP_api_examples/master/Terraform/Terraform_Demo_policy/networks.tf > networks.tf
-curl -kLs https://raw.githubusercontent.com/jimoq/CHKP_api_examples/master/Terraform/Terraform_Demo_policy/policy.tf > policy.tf
+curl -kLs https://raw.githubusercontent.com/jimoq/CHKP_api_examples/master/Terraform/Terraform_Demo_policy/policy.tf > policy.tf.orig
 curl -kLs https://raw.githubusercontent.com/jimoq/CHKP_api_examples/master/Terraform/Terraform_Demo_policy/publish.bat > publish.bat
 curl -kLs https://raw.githubusercontent.com/jimoq/CHKP_api_examples/master/Terraform/Terraform_Demo_policy/publish.py > publish.py
-curl -kLs https://raw.githubusercontent.com/jimoq/CHKP_api_examples/master/Terraform/Terraform_Demo_policy/rules.tf > rules.tf
+curl -kLs https://raw.githubusercontent.com/jimoq/CHKP_api_examples/master/Terraform/Terraform_Demo_policy/rules.tf > rules.tf.orig
 curl -kLs https://raw.githubusercontent.com/jimoq/CHKP_api_examples/master/Terraform/Terraform_Demo_policy/services.tf > services.tf
-echo "Files downloaded to Terraform_Demo_policy directory"
