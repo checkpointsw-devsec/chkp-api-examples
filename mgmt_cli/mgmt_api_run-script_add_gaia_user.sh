e23d97d2cc8bc49ee3d07c34a36175fe8e86281f
Jim Öqvist <44175368+jimoq@users.noreply.github.com>
2020-02-29 21:07:35 +0100

    Update mgmt_api_run-script_add_gaia_user.sh
    changed to readable format

--- mgmt_cli/mgmt_api_run-script_add_gaia_user.sh
+++ mgmt_cli/mgmt_api_run-script_add_gaia_user.sh
@@ -3,7 +3,16 @@
 # https://sc1.checkpoint.com/documents/latest/APIs/index.html#cli/run-script
 # uid jimo
 # pwd admin
-mgmt_cli --root true --format json run-script script-name "Add Gaia user: jimo" script "clish -c 'lock database override'; clish -s -c 'add user jimo uid 0 homedir /home/jimo'; clish -s -c 'set user jimo gid 100 shell /bin/bash'; clish -s -c 'set user jimo password-hash $1$/PAfqAlg$1T9G28s.2jBHq6rFYOdIA1'; clish -s -c 'add rba user jimo roles adminRole'" targets.1 "corporate-gateway" | jq -r '.tasks[]."task-details"[].responseMessage' | base64 --decode -i
+mgmt_cli --root true --format json run-script \
+  script-name "Add Gaia user: jimo" \
+  \
+  script "clish -c 'lock database override';\
+          clish -s -c 'add user jimo uid 0 homedir /home/jimo';\
+          clish -s -c 'set user jimo password-hash $1$/PAfqAlg$1T9G28s.2jBHq6rFYOdIA1';\
+          clish -s -c 'add rba user jimo roles adminRole'" \
+  \
+  targets.1 "corporate-gateway"\
+  | jq -r '.tasks[]."task-details"[].responseMessage' | base64 --decode -i
 
 # The command will produce an output like this
 # ---------------------------------------------
