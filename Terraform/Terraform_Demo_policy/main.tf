38277f6f2aae00ac41484bd689b16e296e74ea50
Jim Öqvist <44175368+jimoq@users.noreply.github.com>
2024-05-02 12:16:06 +0000

    aded s1c parameters

--- Terraform/Terraform_Demo_policy/main.tf
+++ Terraform/Terraform_Demo_policy/main.tf
@@ -9,6 +9,26 @@ terraform {
 
 provider "checkpoint" {
   # Configuration options
+/*
+  # Smart-1 Cloud example
+  server        = "chkp-jim-xxx-nxx22x11.maas.checkpoint.com"
+  api_key       = "XYZbv4ZyxdM+SdtjRI8AmjA=="
+  context       = "web_api"
+  cloud_mgmt_id = "77zz4yxx-c457-4646-8e63-d67e55345af8"
+  timeout       = "120"
+*/
+/*
+  # On premises Management example  
+  server        = "192.168.233.40"
+  #api_key      = ""
+  username      = "api_user"
+  password      = "vpn123"
+  #domain       = "Domain Name"
+  context       = "web_api"
+  timeout       = "120"
+*/
+
+
 }
 
 
