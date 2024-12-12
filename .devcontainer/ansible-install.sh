d73eac66dc91fd3461154515622645ea86b89843
chkp-jimo <jim@oqvist.org>
2024-12-12 13:53:03 +0000

    added installation of pipx and gaia collection for the container

--- .devcontainer/ansible-install.sh
+++ .devcontainer/ansible-install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # use this for consistent results (python dependencies)
+sudo apt install pipx -y
 pipx install --include-deps ansible
 ansible-galaxy collection install check_point.mgmt
+ansible-galaxy collection install check_point.gaia
 
 # THIS DID NOT WORK WELL due to python dependencies - AVOID IT:
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-ansible-on-ubuntu-20-04
